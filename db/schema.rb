cc0ca5893ed6b8445645c373eea7bd13879ffb7a
mudassir-ijaz <ijazmudassir786@gmail.com>
2019-08-21 16:44:05 +0500

    add columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -58,9 +58,13 @@ ActiveRecord::Schema.define(version: 2019_08_21_103440) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
